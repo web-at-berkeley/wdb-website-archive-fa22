976df0989260e342ed96a909635a81f7a98ea0cb
Samarth Goel <sgoel9@berkeley.edu>
2020-05-29 12:31:19 -0500

    ran build for v1

---
+++ build/precache-manifest.6f0546675d06e8bc5ded4debe652b298.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e595c3cad216e35fd49247c8eda14a9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ef030a6371e77468d82a",
+    "url": "/static/css/main.ea131455.chunk.css"
+  },
+  {
+    "revision": "ce6e4ba4ba68da0762b0",
+    "url": "/static/js/2.69721240.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.69721240.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef030a6371e77468d82a",
+    "url": "/static/js/main.f7286637.chunk.js"
+  },
+  {
+    "revision": "2060b9a7e54a59ab7b07",
+    "url": "/static/js/runtime-main.f3a02c50.js"
+  },
+  {
+    "revision": "f9b224fee774de57b7e8d4d4f55e0964",
+    "url": "/static/media/aditring.f9b224fe.jpg"
+  },
+  {
+    "revision": "b28b7403ba824da24b99217a3fb4c983",
+    "url": "/static/media/blobs.b28b7403.png"
+  },
+  {
+    "revision": "e3d1c5d6bec1ebccb37ba0d11dc956c2",
+    "url": "/static/media/blobs.e3d1c5d6.png"
+  },
+  {
+    "revision": "d8141d4c8ac3963e73950f7daeeecb2a",
+    "url": "/static/media/bubbles.d8141d4c.png"
+  },
+  {
+    "revision": "1e184a0d8e86c40428379e73f5e5ad63",
+    "url": "/static/media/ervinring.1e184a0d.jpg"
+  },
+  {
+    "revision": "d3d4c7e6adcdb4bb3d43d0fc690e660f",
+    "url": "/static/media/footer.d3d4c7e6.png"
+  },
+  {
+    "revision": "a56337e0e5a14dc4c1160904c463f82e",
+    "url": "/static/media/formula.a56337e0.jpg"
+  },
+  {
+    "revision": "466b50ac870b07561208b1d6ee94eb8f",
+    "url": "/static/media/frio.466b50ac.jpg"
+  },
+  {
+    "revision": "b6ea6d9e1c4f549ea8348104bd09e82a",
+    "url": "/static/media/graphic.b6ea6d9e.png"
+  },
+  {
+    "revision": "838ee1eb3e45c5d8b99475f7039def2f",
+    "url": "/static/media/grotech.838ee1eb.jpg"
+  },
+  {
+    "revision": "688dea61d10de138e753536a9baa4ee4",
+    "url": "/static/media/justinring.688dea61.jpg"
+  },
+  {
+    "revision": "a8de44d7e2b391b74495ed8aec5b0168",
+    "url": "/static/media/landing-background.a8de44d7.jpg"
+  },
+  {
+    "revision": "b51dade4809b470fd2e827124731d9e0",
+    "url": "/static/media/landing-design-mobile.b51dade4.png"
+  },
+  {
+    "revision": "1ffea0f7f12a8b9c7a94d9518a364a63",
+    "url": "/static/media/landing-design.1ffea0f7.png"
+  },
+  {
+    "revision": "1cafa938302edffa1805cf5c3f06d2f8",
+    "url": "/static/media/leftrings.1cafa938.png"
+  },
+  {
+    "revision": "2a8faefec3251bdfe251b2594fe20802",
+    "url": "/static/media/logo-rainbow.2a8faefe.png"
+  },
+  {
+    "revision": "5ac70fb250cd97654553dcd888dec04b",
+    "url": "/static/media/overlay-rotated.5ac70fb2.png"
+  },
+  {
+    "revision": "57bb6fa0da98b3ccd0a85a8efbbb9579",
+    "url": "/static/media/overlay.57bb6fa0.png"
+  },
+  {
+    "revision": "6fc77bce2ef0492f971aaa6f1caba02c",
+    "url": "/static/media/resource19.6fc77bce.jpg"
+  },
+  {
+    "revision": "2dfc93222d34a617514a8ab4009b47ab",
+    "url": "/static/media/right-nav.2dfc9322.png"
+  },
+  {
+    "revision": "694f185bb3684a4683ed3ad55fb90914",
+    "url": "/static/media/rightrings.694f185b.png"
+  },
+  {
+    "revision": "79416a3811e33afe9420c74dce8697fc",
+    "url": "/static/media/samring.79416a38.jpg"
+  },
+  {
+    "revision": "fe09d967a164d782c9042d59ac8414c0",
+    "url": "/static/media/site-nav-background.fe09d967.png"
+  },
+  {
+    "revision": "af14266c3463ec0e09167fe8bab84468",
+    "url": "/static/media/spider.af14266c.jpg"
+  },
+  {
+    "revision": "96370150f1f7ae578a92e08f36b0bf16",
+    "url": "/static/media/vickyring.96370150.jpg"
+  }
+]);
