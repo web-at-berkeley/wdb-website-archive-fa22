e659f5e289eda58756a9fdd81abf1ade15296213
Samarth Goel <sgoel9@berkeley.edu>
2020-06-02 13:46:37 -0500

    updated error page

--- src/components/404.js
+++ src/components/404.js
@@ -1,22 +1,27 @@
 import React from 'react';
 import { Link } from '@reach/router';
 
+import ErrorImage from '../img/error.png';
+
 const ErrorPage = () => {
 	return (
-		<div
-			style={{
-				display: 'flex',
-				flexDirection: 'column',
-				alignItems: 'center',
-				justifyContent: 'center',
-				height: '100vh',
-			}}
-		>
-			<h1 style={{ fontSize: '72px' }}>404</h1>
-			<Link to="/">
-				<p style={{ fontSize: '26px' }}>Return to Home</p>
-			</Link>
-		</div>
+		<Link to="/">
+			<div
+				style={{
+					display: 'flex',
+					flexDirection: 'column',
+					// alignItems: 'center',
+					// justifyContent: 'center',
+					height: '100vh',
+				}}
+			>
+				<img
+					src={ErrorImage}
+					alt="error"
+					style={{ width: '50%', marginLeft: '1%', marginTop: '.5rem' }}
+				/>
+			</div>
+		</Link>
 	);
 };
 
