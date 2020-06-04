91bb987b920ebac36978ceb8c3957fea5a04181a
Samarth Goel <sgoel9@berkeley.edu>
2020-06-04 15:40:10 -0500

    Created contact modal

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
 
 const Contact = () => {
+	const [show, setShow] = useState(false);
+
+	const handleClose = () => setShow(false);
+	const handleShow = () => setShow(true);
 	return (
-		<div style={style.Container}>
-			<h5 style={style.Header}>Want to Work With Us?</h5>
-			<a href="mailto: webatberkeley@gmail.com">
-				<p style={style.Subtitle}>Contact Us!</p>
-			</a>
+		<div>
+			<div style={style.Container}>
+				<h5 style={style.Header}>Want to Work With Us?</h5>
+				<Button
+					onClick={handleShow}
+					style={{ background: 'transparent', border: 'none' }}
+				>
+					<p style={style.Subtitle}>Contact Us!</p>
+				</Button>
+			</div>
+
+			<Modal show={show} size="lg" onHide={handleClose}>
+				<Modal.Header closeButton></Modal.Header>
+				<Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+			</Modal>
 		</div>
 	);
 };
@@ -21,6 +38,7 @@ let style = {
 	Header: {
 		fontFamily: 'Raleway',
 		fontSize: '31px',
+		fontWeight: '700',
 		letterSpacing: '0.22em',
 		textTransform: 'uppercase',
 		color: '#5F60FF',
