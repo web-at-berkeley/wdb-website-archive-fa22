ca4a98d75a2e3ed2a7b8f4cbb16108cca11887eb
Samarth Goel <sgoel9@berkeley.edu>
2020-06-04 22:59:33 -0500

    built contact modal

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 
 import Modal from 'react-bootstrap/Modal';
-import Button from 'react-bootstrap/Button';
+
+import ContactModal from './ContactModal';
 
 const Contact = () => {
 	const [show, setShow] = useState(false);
@@ -12,82 +13,13 @@ const Contact = () => {
 		<div>
 			<div style={style.Container}>
 				<h5 style={style.Header}>Want to Work With Us?</h5>
-				<Button
-					onClick={handleShow}
-					style={{ background: 'transparent', border: 'none' }}
-				>
-					<p style={style.Subtitle}>Contact Us!</p>
-				</Button>
+				<p style={style.Subtitle} onClick={handleShow} className="cursor">
+					Contact Us!
+				</p>
 			</div>
 
 			<Modal show={show} size="lg" onHide={handleClose}>
-				<Modal.Header closeButton></Modal.Header>
-				<Modal.Body>
-					<form
-						action="https://docs.google.com/forms/u/1/d/e/1FAIpQLSfIhjGYVPkR5ds6lp4-BD2veVE8lVHBCGths4Hl140pQLVIqg/formResponse" 
-						target="_self" 
-						method="POST" 
-						id="mG61Hd"
-					>
-						<div class="name">
-							<p>
-								<label for="first">FIRST NAME</label>
-								<input
-									type="text"
-									required
-									name="entry.453885916"
-									id="first"
-									placeholder="Please enter your first name."
-								/>
-							</p>
-							<p class="last-name">
-								<label for="last">LAST NAME</label>
-								<input
-									type="text"
-									required
-									name="entry.424415708"
-									id="first"
-									placeholder="Please enter your last name."
-								/>
-							</p>
-						</div>
-
-						<p class="email">
-							<label for="email">EMAIL ADDRESS</label>
-							<input
-								type="text"
-								required
-								name="entry.1510521009"
-								id="first"
-								placeholder="Please enter your email address."
-							/>
-						</p>
-						<p class="organization">
-							<label for="organization">ORGANIZATION NAME</label>
-							<input
-								type="text"
-								required
-								name="entry.108998585"
-								id="first"
-								placeholder="Please enter your email address."
-							/>
-						</p>
-						<p>
-							<label for="body">COMMENTS, QUESTIONS, FEEDBACK?</label>
-							<input
-								type="text"
-								required
-								name="entry.1252965523"
-								id="first"
-								placeholder="Please enter your message."
-								class="big"
-							/>
-						</p>
-						<p>
-							<input id="submit" type="submit" value="Send message" />
-						</p>
-					</form>
-				</Modal.Body>
+				<ContactModal />
 			</Modal>
 		</div>
 	);
@@ -114,6 +46,19 @@ let style = {
 		letterSpacing: '0.14em',
 		textDecorationLine: 'underline',
 		color: '#000000',
+	},
+	FormText: {
+		fontSize: '22px',
+		letterSpacing: '0.14em',
+		textTransform: 'uppercase',
+		fontFamily: 'Raleway',
+		fontWeight: '700',
+		marginBottom: '6px',
+	},
+	FormBox: {
+		boxShadow: '4px 4px 30px rgba(0, 0, 0, 0.1)',
+		borderRadius: '11px',
+		border: 'none',
 	},
 };
 
