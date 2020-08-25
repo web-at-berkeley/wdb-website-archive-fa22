a4839f0492b0628981870f74a9752899e6614fa5
Victoria Li <victoria.li@berkeley.edu>
2020-08-25 15:10:04 -0700

    updated apply page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,10 @@
 import React from 'react';
 
 import FooterBackground from '../img/footer.png';
-import Linkedin from '../img/linkedin-white.png';
+import Linkedin from '../img/LinkedIN.png';
+import Facebook from '../img/facebook.png';
+import Instagram from '../img/instagram.png';
+import Mail from '../img/Mail.png';
 import Badge from '../img/badge.png';
 
 import '../css/Footer.css';
@@ -15,20 +18,33 @@ const Footer = () => {
 				draggable="false"
 				className="bg-image selectDisable"
 			/>
-
 			<a
 				href="https://www.linkedin.com/company/web-dev-at-berkeley/"
 				target="_blank"
 				rel="noopener noreferrer"
 			>
-				<img src={Linkedin} alt="Linkedin" className="linkedin" />
+				<img src={Linkedin} alt="Linkedin" className="social linkedin" />
 			</a>
 			<a
 				href="mailto: webatberkeley@gmail.com"
 				target="_blank"
 				rel="noopener noreferrer"
 			>
-				<p className="text">Email: webatberkeley@gmail.com</p>
+				<img src={Mail} alt="Mail" className="social mail" />
+			</a>
+			<a
+				href="https://www.instagram.com/webatberkeley/"
+				target="_blank"
+				rel="noopener noreferrer"
+			>
+				<img src={Instagram} alt="Instagram" className="social instagram" />
+			</a>
+			<a
+				href="https://www.facebook.com/webatberkeley/"
+				target="_blank"
+				rel="noopener noreferrer"
+			>
+				<img src={Facebook} alt="Facebook" className="social facebook" />
 			</a>
 			<a href="/">
 				<img src={Badge} alt="badge" className="badge" />
