0135c9d03c667bb88ea9d53be68beae1c5476308
AdityaBhawal <aditya.bhawal@gmail.com>
2020-12-27 17:50:35 -0800

    Partner data

---
+++ src/data/PartnerData.js
@@ -0,0 +1,34 @@
+import Grotech from '../img/about/partnerships/grotech.png';
+import Formula from '../img/about/partnerships/formula.png';
+import Cloud from '../img/about/partnerships/cloud.png';
+import Tbf from '../img/about/partnerships/tbf.png';
+import Stac from '../img/about/partnerships/stac.png';
+
+export const PartnerData = [
+	{
+		name: 'Cloud at Cal',
+		cover: Cloud,
+		link: 'http://cloudatcal.org/',
+	},
+	{
+		name: 'Formula Electric at Berkeley',
+		cover: Formula,
+		link: 'https://ev.berkeley.edu/',
+	},
+	{
+		name: 'GroTech @ Berkeley',
+		cover: Grotech,
+		link: 'https://www.ocf.berkeley.edu/~grotech/',
+	},
+	{
+		name: 'STAC',
+		cover: Stac,
+		link: 'https://stac.berkeley.edu/',
+	},
+	{
+		name: 'The Berkeley Forum',
+		cover: Tbf,
+		link: 'https://forum.berkeley.edu/',
+	},
+];
+
