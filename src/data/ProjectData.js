87e772bce2dd2bdbe8c4ebd76916bb428ad6fee7
Victoria Li <victoria.li@berkeley.edu>
2020-12-28 17:02:32 -0600

    lowercase fiLENAMES GUYS

--- src/data/ProjectData.js
+++ src/data/ProjectData.js
@@ -1,17 +1,17 @@
-import Resource19 from '../img/projects/Resource19.png';
-import GroTech from '../img/projects/GroTech.png';
-import Frio from '../img/projects/Frio.png';
+import Resource19 from '../img/projects/resource19.png';
+import GroTech from '../img/projects/grotech.png';
+import Frio from '../img/projects/frio.png';
 import FormulaElectric from '../img/projects/FormulaElectric.png';
-import TBF from '../img/projects/TBF.png';
+import TBF from '../img/projects/tbf.png';
 import CloudAtCal from '../img/projects/CloudAtCal.png';
-import Ansari from '../img/projects/Ansari.png';
-import Netic from '../img/projects/Netic.png';
-import TitanRig from '../img/projects/TitanRig.png';
-import Carepath from '../img/projects/Carepath.png';
+import Ansari from '../img/projects/ansari.png';
+import Netic from '../img/projects/netic.png';
+import TitanRig from '../img/projects/titanrig.png';
+import Carepath from '../img/projects/carepath.png';
 import SAAI from '../img/projects/SAAI.png';
-import CMES from '../img/projects/CMES.png';
+import CMES from '../img/projects/cmes.png';
 import UDoItLegal from '../img/projects/UDoItLegal.png';
-import BPT from '../img/projects/BPT.png';
+import BPT from '../img/projects/bpt.png';
 import BarracudaTechnologies from '../img/projects/BarracudaTechnologies.png';
 
 export const Fall2020ProjectData = [
