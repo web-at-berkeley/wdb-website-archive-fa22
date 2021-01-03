49e5143b7c70a59c0fdbeb94d71482e472196631
Samarth Goel <samarthgoel@Samarths-Air.hsd1.il.comcast.net>
2021-01-03 16:59:13 -0600

    t

--- src/components/Project.js
+++ src/components/Project.js
@@ -27,23 +27,37 @@ const Project = ({ project }) => {
 
 	return (
 		<Col sm={12} md={6} lg={4} className="project">
-			<a
-				href={link}
-				title={name}
-				className="cover-link"
-				target="_blank"
-				rel="noopener noreferrer"
-			>
-				<img src={cover} alt={name} className="cover" draggable="false" />
+			{link ? (
+				<a
+					href={link}
+					title={name}
+					className="cover-link"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					<img src={cover} alt={name} className="cover" draggable="false" />
 
-				<div className="overlay image">
-					<img src={Overlay} alt="overlay" draggable="false" />
+					<div className="overlay image">
+						<img src={Overlay} alt="overlay" draggable="false" />
+					</div>
+					<div className="overlay text">
+						<h3>{name}</h3>
+						<p>Created using {description}</p>
+					</div>
+				</a>
+			) : (
+				<div className="cover-link">
+					<img src={cover} alt={name} className="cover" draggable="false" />
+
+					<div className="overlay image">
+						<img src={Overlay} alt="overlay" draggable="false" />
+					</div>
+					<div className="overlay text">
+						<h3>{name}</h3>
+						<p>Created using {description}</p>
+					</div>
 				</div>
-				<div className="overlay text">
-					<h3>{name}</h3>
-					<p>Created using {description}</p>
-				</div>
-			</a>
+			)}
 
 			<div className="description">
 				<h3>{name}</h3>
