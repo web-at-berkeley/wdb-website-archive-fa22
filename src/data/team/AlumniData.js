39e9243ed0a7e385de0dbe9aeabc9f9813950d6b
Samarth Goel <55726218+sgoel97@users.noreply.github.com>
2021-01-05 13:56:40 -0600

    Update AlumniData.js

--- src/data/team/AlumniData.js
+++ src/data/team/AlumniData.js
@@ -5,7 +5,7 @@ export const AlumniData = {
 	Ervin: {
 		name: 'Ervin Baccay',
 		role: 'Software Engineer at Embark',
-		description: "Sometimes I like to go in my bathtub, fill it with milk, and pretend I'm boba",
+		description: "Sometimes I like to go in my bathtub, fill it with milk, and pretend I'm boba.",
 		linkedin: 'https://www.linkedin.com/in/ebaccay/',
 		mail: 'https://github.com/ebaccay',
 		image: Ervin,
