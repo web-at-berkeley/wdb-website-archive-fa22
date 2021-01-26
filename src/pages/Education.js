d9d56fe895bdaa4d4a159feb5cfd19f0dd0643c0
Samarth Goel <samarthgoel@Samarths-Air.hsd1.il.comcast.net>
2021-01-26 01:39:41 -0600

    edu description

--- src/pages/Education.js
+++ src/pages/Education.js
@@ -12,10 +12,6 @@ import RightNav from '../img/right-nav.png';
 import './css/AboutPages.scss';
 
 const Education = () => {
-	let GraphicVersion = Graphic;
-	if (matchMedia('(max-width: 992px)').matches) {
-		GraphicVersion = GraphicMobile;
-	}
 	return (
 		<div className="about-page education">
 			<SiteNavbar />
@@ -36,7 +32,7 @@ const Education = () => {
 				</Row>
 				<Row>
 					<Col xs={12} className="text-center">
-						<p className="subtitle">
+						<p className="edu-subtitle">
 							In our Education Branch, we offer students the unique opportunity
 							to gain a comprehensive web development education in both
 							frontend, UI/UX design, and backend. Students will receive
@@ -45,15 +41,35 @@ const Education = () => {
 							alongside a small cohort of other club members, meaning lessons
 							will be highly personalized to each individual.
 						</p>
+						<p className="edu-subtitle">
+							All students go through our "basic stack", which consists of 3.5
+							weeks of HTML, CSS, JavaScript, and basic networking protocols.
+							From these students choose a specialization in frontend or backend
+							(mostly for logistical purposes, all materials and lectures from
+							both branches are open to all students). The frontend track covers
+							React.js, UI/UX design, along with other miscellaneous frontend
+							technologies such as SASS and Typescript. The backend track covers
+							Node.js, Django, AWS, MongoDB, and PostgreSQL. We aim to offer a
+							full-stack education, so both tracks contain 3 lessons pulled from
+							the other track.
+						</p>
 					</Col>
 				</Row>
 				<Row>
 					<Col xs={12} className="text-center" style={{ padding: '0' }}>
 						<img
-							src={GraphicVersion}
+							src={Graphic}
 							alt="WDB Education Graphic"
 							style={{ width: '100%' }}
 							draggable="false"
+							className="graphic graphic-desktop"
+						/>
+						<img
+							src={GraphicMobile}
+							alt="WDB Education Graphic"
+							style={{ width: '100%' }}
+							draggable="false"
+							className="graphic graphic-mobile"
 						/>
 					</Col>
 				</Row>
