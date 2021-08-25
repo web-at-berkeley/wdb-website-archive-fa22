5ce2ce00d19c90f0b057de7daf0e00e596370240
Samarth Goel <samarthgoel@Samarths-MacBook-Air.local>
2021-08-25 00:48:40 -0700

    update join

--- src/pages/join/index.js
+++ src/pages/join/index.js
@@ -3,11 +3,12 @@ import React from 'react';
 import testimonials from './constants/Testimonials';
 import recruitment from './constants/Recruitment';
 import Header from '../../components/header';
+import Button from '../../components/button';
+
+import { Link } from 'react-router-dom';
 
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
-
-
 
 import Bg from './img/bg.png';
 import Blobs from './img/blobs.png';
@@ -25,13 +26,28 @@ const Join = () => {
 			<Header
 				title="Join the team"
 				subtitle="We're excited to meet you!"
-				
+				description="If you're looking to join either the Introduction to Full-Stack Development DeCal or join Web Development at Berkeley as a Developer or Designer, you're in the right place!"
 			/>
 			<Container className={styles['container']}>
-
 				<div className={styles['recruitment']}>
 					<img src={Blobs2} alt="" />
 					<h3>Recruitment Timeline</h3>
+					<p className={styles['descrip']}>
+						Fill out our interest form to stay up-to-date with our recruitment
+						process! You can find links to our infosession facebook events for
+						both the Introduction to Full-Stack DeCal and Web Development at
+						Berkley below. You can also find links to the official applications
+						below.
+					</p>
+					<div className={styles['button-interest']}>
+						<a
+							href="https://tinyurl.com/wdb-if"
+							target="_blank"
+							rel="noreferrer"
+						>
+							<Button text="Club + DeCal Interest Form" />
+						</a>
+					</div>
 					{/* {recruitment.map(({ title, location, description }) => (
 						<div className={styles['timeline']}>
 							<p className={styles['title']}>{title}</p>
@@ -40,38 +56,187 @@ const Join = () => {
 						</div>
 					))} */}
 					<div className={styles['timeline']}>
-							<p className={styles['title']}>Applications open - <span className={styles['date-time']}>Aug 25, 11:59 PM</span></p>
-							<p className={styles['location']}>📍 Remote</p>
-							<p className={styles['text']}>Link to our application coming soon! In the application you will find information about roles,  time commitments, organization structure and more.</p>
+						<p className={styles['title']}>
+							Applications open -{' '}
+							<span className={styles['date-time']}>Aug 24, 11:59 PM</span>
+						</p>
+						<p className={styles['location']}>📍 Remote</p>
+						<p className={styles['text']}>
+							In the application you will find information about roles, time
+							commitments, organization structure and more.
+						</p>
+
+						<div className={styles['button-temp']}>
+							<a
+								href="https://forms.gle/CA9pykbRcRNizEQz7"
+								target="_blank"
+								rel="noreferrer"
+								style={{ marginBottom: '1.5rem' }}
+							>
+								<Button text="Developer + Designer Application" />
+							</a>
+
+							<a
+								href="https://forms.gle/6YdV44MgbN8PfsFt6"
+								target="_blank"
+								rel="noreferrer"
+							>
+								<Button text="Full-Stack DeCal Application" />
+							</a>
 						</div>
+					</div>
 
 					<div className={styles['timeline']}>
-							<p className={styles['title']}>Infosession - <span className={styles['date-time']}>Sept 2, 7PM pst - 8PM PST</span></p>
-							<p className={styles['location']}>📍 Jacobs 310</p>
-							<p className={styles['text']}>Come to our infosession to meet our members, listen to what we’re all about, and hear about exciting plans for the upcoming semester! This will be recorded as well.</p>
+						<p className={styles['title']}>
+							Full-Stack DeCal Infosession #1 -{' '}
+							<span className={styles['date-time']}>
+								Aug 30, 7PM pst - 9PM PST
+							</span>
+						</p>
+						<p className={styles['location']}>📍 TBD</p>
+						<p className={styles['text']}>
+							Come to our infosession to learn more about the Introduction to
+							Full-Stack Development DeCal.
+						</p>
+						<div className={styles['button-temp']}>
+							<a
+								href="https://fb.me/e/1Gb6KeEuU"
+								target="_blank"
+								rel="noreferrer"
+							>
+								<Button text="Full-Stack DeCal Infosession One" />
+							</a>
 						</div>
-					<div className={styles['timeline']}>
-							<p className={styles['title']}>Applications Close - <span className={styles['date-time']}>Sept 5, 11:59 PM</span></p>
-							<p className={styles['location']}>📍 Remote</p>
-							<p className={styles['text']}>Make sure you get it in before out 11:59 PM cutoff time in order to secure your application to our organization.</p>
-						</div>
-					<div className={styles['timeline']}>
-							<p className={styles['title']}>Behaviorial Interviews (invite only) - <span className={styles['date-time']}>Sept 7 - Sept 8</span></p>
-							<p className={styles['location']}>📍 TBD</p>
-							<p className={styles['text']}>Interviews for WDB Product Bootcamp and Industry Initiatives</p>
-						</div>
-					<div className={styles['timeline']}>
-							<p className={styles['title']}>Technical Interviews (invite only) - <span className={styles['date-time']}>Sept 12</span></p>
-							<p className={styles['location']}>📍 TBD</p>
-							<p className={styles['text']}>Technical Interviews (technical project presentations).</p>
-						</div>
-						<div className={styles['timeline']}>
-							<p className={styles['title']}>First Meeting (invite only) - <span className={styles['date-time']}>Sept 15, 8PM PST - 10PM PST</span></p>
-							<p className={styles['location']}>📍 TBD</p>
-							<p className={styles['text']}>Our first all-team meeting with new and returning members.</p>
-						</div>
+					</div>
 
+					<div className={styles['timeline']}>
+						<p className={styles['title']}>
+							WDB Infosession #1 -{' '}
+							<span className={styles['date-time']}>
+								Aug 31, 7PM pst - 9PM PST
+							</span>
+						</p>
+						<p className={styles['location']}>📍 TBD</p>
+						<p className={styles['text']}>
+							Come to our infosession to meet our members, listen to what we’re
+							all about, and hear about exciting plans for the upcoming
+							semester! This will be recorded as well.
+						</p>
+						<div className={styles['button-temp']}>
+							<a
+								href="https://fb.me/e/1ed4MKUgl"
+								target="_blank"
+								rel="noreferrer"
+							>
+								<Button text="WDB Infosession One" />
+							</a>
+						</div>
+					</div>
 
+					<div className={styles['timeline']}>
+						<p className={styles['title']}>
+							Full-Stack DeCal Infosession #2 -{' '}
+							<span className={styles['date-time']}>
+								Sept 1, 7PM pst - 9PM PST
+							</span>
+						</p>
+						<p className={styles['location']}>📍 TBD</p>
+						<p className={styles['text']}>
+							Come to our infosession to learn more about the Introduction to
+							Full-Stack Development DeCal. This infosession will cover the same
+							content as the first one, so no need to come to both!
+						</p>
+						<div className={styles['button-temp']}>
+							<a
+								href="https://fb.me/e/1FA1KEDbk"
+								target="_blank"
+								rel="noreferrer"
+							>
+								<Button text="Full-Stack DeCal Infosession One" />
+							</a>
+						</div>
+					</div>
+
+					<div className={styles['timeline']}>
+						<p className={styles['title']}>
+							WDB Infosession #2 -{' '}
+							<span className={styles['date-time']}>
+								Sept 2, 7PM pst - 8PM PST
+							</span>
+						</p>
+						<p className={styles['location']}>📍 Jacobs 310</p>
+						<p className={styles['text']}>
+							Come to our second infosession to meet our members, listen to what
+							we’re all about, and hear about exciting plans for the upcoming
+							semester! This will be recorded as well. This infosession will
+							cover the same content as the first one, so no need to come to
+							both!
+						</p>
+						<div className={styles['button-temp']}>
+							<a
+								href="https://fb.me/e/1ed4MKUgl"
+								target="_blank"
+								rel="noreferrer"
+							>
+								<Button text="WDB Infosession Two" />
+							</a>
+						</div>
+					</div>
+					<div className={styles['timeline']}>
+						<p className={styles['title']}>
+							Developer + Designer Applications Close -{' '}
+							<span className={styles['date-time']}>Sept 5, 11:59 PM</span>
+						</p>
+						<p className={styles['location']}>📍 Remote</p>
+						<p className={styles['text']}>
+							Make sure you get it in before out 11:59 PM cutoff time in order
+							to secure your application to our organization.
+						</p>
+					</div>
+
+					<div className={styles['timeline']}>
+						<p className={styles['title']}>
+							Full Stack DeCal Applications Close -{' '}
+							<span className={styles['date-time']}>Sept 7, 11:59 PM</span>
+						</p>
+						<p className={styles['location']}>📍 Remote</p>
+						<p className={styles['text']}>
+							Make sure you get it in before out 11:59 PM cutoff time in order
+							to secure your application to the DeCal.
+						</p>
+					</div>
+					<div className={styles['timeline']}>
+						<p className={styles['title']}>
+							Behaviorial Interviews (invite only) -{' '}
+							<span className={styles['date-time']}>Sept 7 - Sept 8</span>
+						</p>
+						<p className={styles['location']}>📍 TBD</p>
+						<p className={styles['text']}>
+							Interviews for WDB Product Bootcamp and Industry Initiatives
+						</p>
+					</div>
+					<div className={styles['timeline']}>
+						<p className={styles['title']}>
+							Technical Interviews (invite only) -{' '}
+							<span className={styles['date-time']}>Sept 12</span>
+						</p>
+						<p className={styles['location']}>📍 TBD</p>
+						<p className={styles['text']}>
+							Technical Interviews (technical project presentations).
+						</p>
+					</div>
+					<div className={styles['timeline']}>
+						<p className={styles['title']}>
+							First Meeting (invite only) -{' '}
+							<span className={styles['date-time']}>
+								Sept 15, 8PM PST - 10PM PST
+							</span>
+						</p>
+						<p className={styles['location']}>📍 TBD</p>
+						<p className={styles['text']}>
+							Our first all-team meeting with new and returning members.
+						</p>
+					</div>
 				</div>
 				{/* <div className={styles['interested']}>
 					<img src={Bg} alt="" />
@@ -94,37 +259,51 @@ const Join = () => {
 						</div>
 					))}
 				</div> */}
-				
+
 				<div className={styles['experiences']}>
 					<img className={styles['blob']} src={Blobs3} alt="" />
 					<h3>Member Experiences</h3>
 
 					<Row lg="3" med="1" sm="1" className={styles['experience-group']}>
 						<div className={styles['testimonial1']}>
-							<img src={lizzie} alt='{role.title}' />
-							<p className={styles['text']}>"WDB has allowed me to become a design sponge! Exploring my interests in front-end design was something I gathered the courage to pursue thanks to WDB and the support from fellow designers as well as the freedom to learn design freely has been fire!"</p>
+							<img src={lizzie} alt="{role.title}" />
+							<p className={styles['text']}>
+								"WDB has allowed me to become a design sponge! Exploring my
+								interests in front-end design was something I gathered the
+								courage to pursue thanks to WDB and the support from fellow
+								designers as well as the freedom to learn design freely has been
+								fire!"
+							</p>
 							<p className={styles['title']}>Lizzie ‘24, Designer</p>
 						</div>
 						<div className={styles['testimonial2']}>
-							<img src={anjan} alt='{role.title}' />
+							<img src={anjan} alt="{role.title}" />
 							{/* <p className={styles['title']}>Anjan</p> */}
-							<p className={styles['text']}>"The main reason I wanted to join WDB at first was purely because I had never really gotten the chance to dabble in web development, and I thought this would be a great opportunity for me to actually nail down the fundamentals and then transition into working hands-on on projects, something that’s pretty unique to WDB. Later on I realized a lot of the friends I’ve made at Berkeley have been through this club, WDB has been a really important part of my Berkeley experience thus far :)"</p>
+							<p className={styles['text']}>
+								"The main reason I wanted to join WDB at first was purely
+								because I had never really gotten the chance to dabble in web
+								development, and I thought this would be a great opportunity for
+								me to actually nail down the fundamentals and then transition
+								into working hands-on on projects, something that’s pretty
+								unique to WDB. Later on I realized a lot of the friends I’ve
+								made at Berkeley have been through this club, WDB has been a
+								really important part of my Berkeley experience thus far :)"
+							</p>
 							<p className={styles['title']}>Anjan ‘24, Full Stack Developer</p>
 						</div>
 						<div className={styles['testimonial1']}>
+							<img src={caelin} alt="{role.title}" />
 
-
-							
-							<img src={caelin} alt='{role.title}' />
-							
-							<p className={styles['text']}>"The development branch has exposed me to awesome clients and developers. It’s been a great opportunity to expand my personal network and meet some incredible people. I’ve also had a great time exploring new technology stacks and seeing how others think."</p>
+							<p className={styles['text']}>
+								"The development branch has exposed me to awesome clients and
+								developers. It’s been a great opportunity to expand my personal
+								network and meet some incredible people. I’ve also had a great
+								time exploring new technology stacks and seeing how others
+								think."
+							</p>
 							<p className={styles['title']}>Caelin '24, Product Manager</p>
 						</div>
 					</Row>
-					
-					
-					
-				
 				</div>
 			</Container>
 		</main>
