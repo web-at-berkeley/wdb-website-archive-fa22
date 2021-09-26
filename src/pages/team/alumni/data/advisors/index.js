a9a3c81231b19b4ce9aca80fd115ea69811ef2f6
Samarth Goel <samarthgoel@Samarths-MacBook-Air.local>
2021-09-25 18:14:01 -0700

    armam

--- src/pages/team/alumni/data/advisors/index.js
+++ src/pages/team/alumni/data/advisors/index.js
@@ -10,7 +10,6 @@ import James from './img/james.png';
 import Henry from './img/henry.png';
 import Nerissa from './img/nerissa.png';
 import Sooyeon from './img/sooyeon.png';
-import Armaan from './img/armaan.png';
 
 const data = [
 	{
@@ -35,13 +34,6 @@ const data = [
 		personal: 'https://izzielau.github.io/',
 	},
 	{
-		name: 'Armaan Goel',
-		image: Armaan,
-		role: 'Senior Advisor',
-		linkedin: 'https://www.linkedin.com/in/armaangoel/',
-		personal: 'https://waiting-ixora-7ae.notion.site/Armaan-s-Portfolio-53ab609870ad4397916149ff3273b7be',
-	},
-	{
 		name: 'Albert Tian',
 		image: Albert,
 		role: 'Product Manager',
@@ -55,7 +47,7 @@ const data = [
 		linkedin: 'https://www.linkedin.com/in/andybojiang/',
 		personal: 'http://github.com/andybojiang',
 	},
-	
+
 	{
 		name: 'Neha Haq',
 		image: Neha,
@@ -100,7 +92,7 @@ const data = [
 	{
 		name: 'Sooyeon Oh',
 		image: Sooyeon,
-		role: 'Product Designer',
+		role: 'Designer',
 		linkedin: 'https://www.linkedin.com/in/sooyeonoh',
 	},
 ];
