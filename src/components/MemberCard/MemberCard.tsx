fe8c86e5f893b8f957810e2ad08b94c52dcbeca7
Nico Galin <nico.galin@gmail.com>
2022-11-11 00:43:24 -0800

    Updated PFPs

--- src/components/MemberCard/MemberCard.tsx
+++ src/components/MemberCard/MemberCard.tsx
@@ -38,30 +38,44 @@ const MemberCard = ({ member }: MemberCard_Props) => {
 			<div className={styles['container']}>
 				<img
 					className={styles['photo']}
-					src={!!image ? image : ''}
+					src={
+						!!image
+							? image
+							: require('../../assets/pfps/pfp-default.jpg').default
+					}
 					alt={`${member.firstName} Profile`}
 				/>
-				<div className={styles['socials']}>
-					{!!member.linkedIn && (
-						<a href={member.linkedIn} target="_blank" rel="noopener noreferrer">
-							<img src={LinkedInIcon} alt={`${member.firstName} LinkedIn`} />
-						</a>
-					)}
-					{!!member.website && (
-						<a href={member.website} target="_blank" rel="noopener noreferrer">
-							<img src={GlobeIcon} alt={`${member.firstName} Website`} />
-						</a>
-					)}
-					{!!member.email && (
-						<a
-							href={`mailto:${member.email}`}
-							target="_blank"
-							rel="noopener noreferrer"
-						>
-							<img src={MailIcon} alt={`${member.firstName} Email`} />
-						</a>
-					)}
-				</div>
+				{(!!member.linkedIn || !!member.email || !!member.website) && (
+					<div className={styles['socials']}>
+						{!!member.linkedIn && (
+							<a
+								href={member.linkedIn}
+								target="_blank"
+								rel="noopener noreferrer"
+							>
+								<img src={LinkedInIcon} alt={`${member.firstName} LinkedIn`} />
+							</a>
+						)}
+						{!!member.website && (
+							<a
+								href={member.website}
+								target="_blank"
+								rel="noopener noreferrer"
+							>
+								<img src={GlobeIcon} alt={`${member.firstName} Website`} />
+							</a>
+						)}
+						{!!member.email && (
+							<a
+								href={`mailto:${member.email}`}
+								target="_blank"
+								rel="noopener noreferrer"
+							>
+								<img src={MailIcon} alt={`${member.firstName} Email`} />
+							</a>
+						)}
+					</div>
+				)}
 			</div>
 
 			<h5 className={styles['name']} id="name">
