26b6778efbe18bf54f032c21c833ec4ca185dd20
Victor Shen <victorshen4444@gmail.com>
2024-01-16 15:58:11 -0800

    sp24 team and join

--- src/static/parse-members.py
+++ src/static/parse-members.py
@@ -2,25 +2,73 @@ import csv
 import datetime
 import json
 import uuid
+import requests
+import re
 
-ROLE_COL_NAME = "SP23 Role(s)"
+'''
+TO UPDATE TEAM PAGE:
+- export a csv from the roster AirTable with old and current members, put it into roster.csv
+- must have these columns Name,First Name,Last Name,Nickname,Photo,Berkeley Email,LinkedIn,Personal Website,SP24 Role,Method of Leaving
+- run this script which generates members.json as well as downloads photos to src/assets/pfps
+- go and manually re save any photos with HEIC to jpeg
+
+'''
+
+
+def extract_url_and_filetype(text):
+    # Regular expression pattern to extract the file type
+    filetype_pattern = r'\.\w+'
+
+    # Regular expression pattern to extract URL
+    url_pattern = r'https?://[^\s]+'
+
+    # Find file type
+    filetypes = re.findall(filetype_pattern, text)
+    # Remove the dot at the beginning
+    filetype = filetypes[0][1:] if filetypes else None
+
+    # Find URL
+    urls = re.findall(url_pattern, text)
+    url = urls[0] if urls else None
+
+    return filetype, url
+
+
+def download_image(image_url, image_name):
+    # Send a GET request to the image URL
+    if not image_url:
+        return
+    response = requests.get(image_url)
+
+    # Check if the request was successful
+    if response.status_code == 200:
+        # Write the image to a file
+        with open("../assets/pfps/" + image_name, 'wb') as file:
+            file.write(response.content)
+        # print(f"Image downloaded: {image_name}")
+    else:
+        print(f"Failed to download image: {image_name}")
+
+
+ROLE_COL_NAME = "SP24 Role"
 LINKEDIN_COL_NAME = "LinkedIn"
 WEBSITE_COL_NAME = "Personal Website"
 METHOD_OF_LEAVE_COL_NAME = "Method of Leaving"
 EMAIL_COL_NAME = "Berkeley Email"
+PHOTO_COL_NAME = "Photo"
 
 # 1. Parse raw CSV into JSON for all members (including alumni)
 (
     roleColIndex,
     linkedInColIndex,
     websiteColIndex,
-    birthdayColIndex,
     emailColIndex,
     methodOfLeavingColIndex,
+    photoIndex,
 ) = (-1, -1, -1, -1, -1, -1)
 columns = []
 data = {}
-with open("roster-sp23.csv", encoding="utf8") as csv_file:
+with open("roster.csv", encoding="utf8") as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=",")
     parsedHeader = False
 
@@ -32,12 +80,12 @@ with open("roster-sp23.csv", encoding="utf8") as csv_file:
             websiteColIndex = row.index(WEBSITE_COL_NAME)
             emailColIndex = row.index(EMAIL_COL_NAME)
             methodOfLeavingColIndex = row.index(METHOD_OF_LEAVE_COL_NAME)
+            photoIndex = row.index(PHOTO_COL_NAME)
             parsedHeader = True
         else:
             firstName = "".join(row[1].strip().split(" "))
             lastName = "".join(row[2].strip().split(" "))
             nickname = row[3].strip()
-            birthday = row[birthdayColIndex].strip()
             methodOfLeaving = row[methodOfLeavingColIndex].strip()
 
             # Create an index in the case of duplicate names
@@ -51,17 +99,22 @@ with open("roster-sp23.csv", encoding="utf8") as csv_file:
 
             # Skip the current member if they were removed from the club (left and didnt graduate)
             if methodOfLeaving != "" and (
-                methodOfLeaving != "Graduated" or methodOfLeaving != "Personal Choice"
+                "Graduated" not in methodOfLeaving and "Personal Choice" not in methodOfLeaving
             ):
                 continue
+            photo = row[photoIndex]
+            file_type, url = extract_url_and_filetype(row[photoIndex])
+            download_image(url, "{fN}-{lN}-{ind}.{file_type}".format(
+                fN=firstName.lower(), lN=lastName.lower(), ind=index, file_type=file_type
+            ))
 
-            # Key users based on a UUID
+            # Key users based on a UUIDpyt
             data[str(uuid.uuid4())] = {
                 "firstName": firstName,
                 "lastName": lastName,
                 "nickname": nickname,
-                "image": "{fN}-{lN}-{ind}.jpg".format(
-                    fN=firstName.lower(), lN=lastName.lower(), ind=index
+                "image": "{fN}-{lN}-{ind}.{file_type}".format(
+                    fN=firstName.lower(), lN=lastName.lower(), ind=index, file_type=(file_type if file_type != "heic" and file_type != "HEIC" and file_type != "pdf" else "jpeg")
                 ),
                 "roles": row[roleColIndex].split(","),
                 "linkedIn": row[linkedInColIndex],
@@ -82,7 +135,6 @@ teams = {
     "designTeam": [],
     "externalTeam": [],
 }
-
 for key, member in data.items():
     for i in range(0, len(member["roles"])):
         role = member["roles"][i]
@@ -99,7 +151,7 @@ for key, member in data.items():
             if key not in teams["industryMembers"]:
                 # Not PMs because they were handled in an earlier case
                 teams["industryMembers"].append(key)
-        elif "[DeCal]" in role:
+        elif "[DeCal]" in role or "[Decal]" in role:
             if key not in teams["courseStaff"]:
                 teams["courseStaff"].append(key)
         elif "[Bootcamp]" in role:
